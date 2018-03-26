b9b86f3093d9068610714aef275ddfadc5d75789
Dirk O. Kaar <dok@dok-net.net>
2018-03-26 15:36:17 +0200

    Pull in sensor handling improvements and fixes from doknet/airrohr-firmware fork.

--- Sds011.cpp
+++ Sds011.cpp
@@ -4,224 +4,242 @@ Sds011::Sds011(Stream& out) : _out(out)
 {
 }
 
-String Sds011::firmware_version()
+bool Sds011::device_info(String& firmware_version, String& device_id)
 {
-	bool ok;
+    _send_cmd(CMD_FIRMWARE, NULL, 0);
+    delay(200);
 
-	_send_cmd(CMD_FIRMWARE, NULL, 0);
+    bool ok = _read_response(CMD_FIRMWARE);
+    if (!(ok && crc_ok())) {
+        return false;
+    }
 
-	ok = _read_response();
-	if (!ok) {
-		return "";
-	}
-
-	return String(_buf[3]) + "_" + String(_buf[4]) + "_" + String(_buf[5]);
+    firmware_version = String(_buf[3]) + "-" + String(_buf[4]) + "-" + String(_buf[5]);
+    device_id = (_buf[6] < 0x10) ? String("0") + String(_buf[6], HEX) : String(_buf[6], HEX);
+    device_id += (_buf[7] < 0x10) ? String("0") + String(_buf[7], HEX) : String(_buf[7], HEX);
+    return true;
 }
 
-void Sds011::set_mode(Report_mode mode)
+bool Sds011::set_mode(Report_mode mode)
 {
-	uint8_t data[] = { 0x1, mode };
-	_send_cmd(CMD_MODE, data, 2);
-	_ignore_response();
+    uint8_t data[] = { 0x1, mode };
+    _send_cmd(CMD_MODE, data, 2);
+    delay(200);
+    return _read_response(CMD_MODE) && crc_ok() && _buf[3] == 0x1 && _buf[4] == mode;
 }
 
-void Sds011::set_sleep(bool sleep)
+bool Sds011::set_sleep(bool sleep)
 {
-	uint8_t data[] = { 0x1, !sleep };
-	_send_cmd(CMD_SLEEP, data, 2);
-	_ignore_response();
+    uint8_t data[] = { 0x1, !sleep };
+    _send_cmd(CMD_SLEEP, data, 2);
+    delay(200);
+    return _read_response(CMD_SLEEP) && crc_ok() && !_buf[4] == sleep;
 }
 
 bool Sds011::query_data(int& pm25, int& pm10)
 {
-	bool ok;
-	_send_cmd(CMD_QUERY_DATA, NULL, 0);
+    _send_cmd(CMD_QUERY_DATA, NULL, 0);
+    delay(200);
+    bool ok = _read_response(CMD_QUERY_DATA);
+    if (!ok || !crc_ok()) {
+        return false;
+    }
 
-	ok = _read_response();
-	if (!ok) {
-		return false;
-	}
+    pm25 = _buf[2] | (static_cast<unsigned int>(_buf[3]) << 8);
+    pm10 = _buf[4] | (static_cast<unsigned int>(_buf[5]) << 8);
+    return true;
+}
 
-	pm25 = _buf[2] | _buf[3] << 8;
-	pm10 = _buf[4] | _buf[5] << 8;
+bool Sds011::query_data(int& pm25, int& pm10, int n)
+{
+    int pm25_table[n];
+    int pm10_table[n];
 
-	return true;
+    for (int i = 0; n > 0 && i < n; ++i) {
+        bool ok = query_data(pm25_table[i], pm10_table[i]);
+        if (!ok) {
+            --n;
+            --i;
+            continue;
+        }
+        delay(1000);
+    }
+
+    _filter_data(n, pm25_table, pm10_table, pm25, pm10);
+    return n > 0;
+}
+
+bool Sds011::query_data_auto(int& pm25, int& pm10)
+{
+    bool ok = _read_response(CMD_QUERY_DATA);
+    if (!ok || !crc_ok()) {
+        return false;
+    }
+
+    pm25 = _buf[2] | (static_cast<unsigned int>(_buf[3]) << 8);
+    pm10 = _buf[4] | (static_cast<unsigned int>(_buf[5]) << 8);
+    return true;
 }
 
 bool Sds011::query_data_auto(int& pm25, int& pm10, int n)
 {
-	int pm25_table[n];
-	int pm10_table[n];
-	int ok;
+    int pm25_table[n];
+    int pm10_table[n];
 
-	for (int i = 0; i < n; ++i) {
-		ok = query_data(pm25_table[i], pm10_table[i]);
-		if (!ok) {
-			return false;
-		}
+    for (int i = 0; n > 0 && i < n; ++i) {
+        bool ok = query_data_auto(pm25_table[i], pm10_table[i]);
+        if (!ok) {
+            --n;
+            --i;
+            continue;
+        }
+        delay(1000);
+    }
 
-		ok = crc_ok();
-		if (!ok) {
-			--n;
-			--i;
-			continue;
-		}
-		delay(1000);
-	}
-
-	_filter_data(n, pm25_table, pm10_table, pm25, pm10);
-
-	return true;
+    _filter_data(n, pm25_table, pm10_table, pm25, pm10);
+    return n > 0;
 }
 
 bool Sds011::crc_ok()
 {
-	uint8_t crc = 0;
-	for (int i = 2; i < 8; ++i) {
-		crc += _buf[i];
-	}
-	return crc == _buf[8];
+    uint8_t crc = 0;
+    for (int i = 2; i < 8; ++i) {
+        crc += _buf[i];
+    }
+    return crc == _buf[8];
 }
 
 bool Sds011::timeout()
 {
-	return _timeout;
+    return _timeout;
 }
 
 void Sds011::_send_cmd(enum Command cmd, const uint8_t* data, uint8_t len)
 {
-	uint8_t i;
-	uint8_t crc;
+    uint8_t i;
+    uint8_t crc;
 
-	_buf[0] = 0xAA;
-	_buf[1] = 0xB4;
-	_buf[2] = cmd;
-	_buf[15] = 0xff;
-	_buf[16] = 0xff;
-	_buf[18] = 0xAB;
+    _buf[0] = 0xAA;
+    _buf[1] = 0xB4;
+    _buf[2] = cmd;
+    _buf[15] = 0xff;
+    _buf[16] = 0xff;
+    _buf[18] = 0xAB;
 
-	crc = cmd + _buf[15] + _buf[16];
+    crc = cmd + _buf[15] + _buf[16];
 
-	for (i = 0; i < 12; ++i) {
-		if (i < len) {
-			_buf[3 + i] = data[i];
-		}
-		else {
-			_buf[3 + i] = 0;
-		}
-		crc += _buf[3 + i];
-	}
+    for (i = 0; i < 12; ++i) {
+        if (i < len) {
+            _buf[3 + i] = data[i];
+        }
+        else {
+            _buf[3 + i] = 0;
+        }
+        crc += _buf[3 + i];
+    }
 
-	_buf[17] = crc;
+    _buf[17] = crc;
 
-	for (i = 0; i < 19; ++i) {
-		_out.write(_buf[i]);
-	}
-	_out.flush();
+    _out.write(_buf, sizeof(_buf));
 }
 
-uint8_t Sds011::_read_byte(uint16_t timeout)
+uint8_t Sds011::_read_byte(long unsigned deadline)
 {
-	uint16_t c = 0;
+    while (!_out.available()) {
+        if (deadline > 0 && millis() > deadline) {
+            _timeout = true;
+            return 0;
+        }
+        delay(1);
+    }
 
-	while (!_out.available()) {
-		if (timeout > 0) {
-			if (c == timeout) {
-				_timeout = true;
-				return 0;
-			}
-			c++;
-		}
-		delay(1);
-	}
-
-	_timeout = false;
-	return _out.read();
+    _timeout = false;
+    return _out.read();
 }
 
 void Sds011::_ignore_response()
 {
-	delay(300);
-	while (_out.available())
-		_out.read();
+    delay(200);
+    while (_out.available())
+        _out.read();
 }
 
-bool Sds011::_read_response()
+bool Sds011::_read_response(enum Command cmd)
 {
-	uint8_t i = 1;
-	uint8_t b;
+    uint8_t i = 0;
+    long unsigned deadline = millis() + 1000;
+    while (i < 3) {
+        _buf[i] = _read_byte(deadline);
+        if (timeout()) return false;
+        switch (i++) {
+        case 0: if (_buf[0] != 0xAA) i = 0; break;
+        case 1: if (_buf[1] != ((cmd == CMD_QUERY_DATA) ? 0xC0 : 0xC5)) i = 0; break;
+        case 2: if (cmd != CMD_QUERY_DATA && _buf[2] != cmd) i = 0; break;
+        }
+    }
+    for (i = 3; i < 10; i++) {
+        _buf[i] = _read_byte(deadline);
+    }
 
-	while ((b = _read_byte(1000)) != 0xAA) {
-		if (timeout()) {
-			return false;
-		}
-	}
-
-	_buf[0] = b;
-
-	for (i = 1; i < 10; i++) {
-		_buf[i] = _read_byte(1000);
-	}
-
-	return !timeout();
+    return !timeout() && _buf[9] == 0xAB;
 }
 
 String Sds011::_buf_to_string(void)
 {
-	String ret = "";
-	uint8_t i = 0;
+    String ret = "";
+    uint8_t i = 0;
 
-	for (i = 0; i < 19; i++) {
-		char c = (_buf[i] >> 4) + '0';
-		if (c > '9') {
-			c += 'A' - '9' - 1;
-		}
-		ret += c;
+    for (i = 0; i < 19; i++) {
+        char c = (_buf[i] >> 4) + '0';
+        if (c > '9') {
+            c += 'A' - '9' - 1;
+        }
+        ret += c;
 
-		c = (_buf[i] & 0xf) + '0';
-		if (c > '9') {
-			c += 'A' - '9' - 1;
-		}
-		ret += c;
-	}
+        c = (_buf[i] & 0xf) + '0';
+        if (c > '9') {
+            c += 'A' - '9' - 1;
+        }
+        ret += c;
+    }
 
-	return ret;
+    return ret;
 }
 
 void Sds011::_filter_data(int n, const int* pm25_table, const int* pm10_table, int& pm25, int& pm10)
 {
-	int pm25_min, pm25_max, pm10_min, pm10_max, pm25_sum, pm10_sum;
+    int pm25_min, pm25_max, pm10_min, pm10_max, pm25_sum, pm10_sum;
 
-	pm10_sum = pm10_min = pm10_max = pm10_table[0];
-	pm25_sum = pm25_min = pm25_max = pm25_table[0];
+    pm10_sum = pm10_min = pm10_max = pm10_table[0];
+    pm25_sum = pm25_min = pm25_max = pm25_table[0];
 
-	for (int i = 1; i < n; ++i) {
-		if (pm10_table[i] < pm10_min) {
-			pm10_min = pm10_table[i];
-		}
-		if (pm10_table[i] > pm10_max) {
-			pm10_max = pm10_table[i];
-		}
-		if (pm25_table[i] < pm25_min) {
-			pm25_min = pm25_table[i];
-		}
-		if (pm25_table[i] > pm25_max) {
-			pm25_max = pm25_table[i];
-		}
-		pm10_sum += pm10_table[i];
-		pm25_sum += pm25_table[i];
-	}
+    for (int i = 1; i < n; ++i) {
+        if (pm10_table[i] < pm10_min) {
+            pm10_min = pm10_table[i];
+        }
+        if (pm10_table[i] > pm10_max) {
+            pm10_max = pm10_table[i];
+        }
+        if (pm25_table[i] < pm25_min) {
+            pm25_min = pm25_table[i];
+        }
+        if (pm25_table[i] > pm25_max) {
+            pm25_max = pm25_table[i];
+        }
+        pm10_sum += pm10_table[i];
+        pm25_sum += pm25_table[i];
+    }
 
-	if (n > 2) {
-		pm10 = (pm10_sum - pm10_max - pm10_min) / (n - 2);
-		pm25 = (pm25_sum - pm25_max - pm25_min) / (n - 2);
-	}
-	else if (n > 1) {
-		pm10 = (pm10_sum - pm10_min) / (n - 1);
-		pm25 = (pm25_sum - pm25_min) / (n - 1);
-	}
-	else {
-		pm10 = pm10_sum / n;
-		pm25 = pm25_sum / n;
-	}
+    if (n > 2) {
+        pm10 = (pm10_sum - pm10_max - pm10_min) / (n - 2);
+        pm25 = (pm25_sum - pm25_max - pm25_min) / (n - 2);
+    }
+    else if (n > 1) {
+        pm10 = (pm10_sum - pm10_min) / (n - 1);
+        pm25 = (pm25_sum - pm25_min) / (n - 1);
+    }
+    else {
+        pm10 = pm10_sum / n;
+        pm25 = pm25_sum / n;
+    }
 }
--- Sds011.h
+++ Sds011.h
@@ -13,40 +13,42 @@
 class Sds011
 {
 public:
-	enum Command {
-		CMD_MODE = 2,
-		CMD_QUERY_DATA = 4,
-		CMD_DEVICE_ID,
-		CMD_SLEEP,
-		CMD_FIRMWARE,
-		CMD_WORKING_PERIOD
-	};
+    enum Command {
+        CMD_MODE = 2,
+        CMD_QUERY_DATA = 4,
+        CMD_DEVICE_ID,
+        CMD_SLEEP = 6,
+        CMD_FIRMWARE = 7,
+        CMD_WORKING_PERIOD
+    };
 
-	enum Report_mode {
-		ACTIVE = 0,
-		QUERY = 1
-	};
+    enum Report_mode {
+        ACTIVE = 0,
+        QUERY = 1
+    };
 
-	Sds011(Stream& out);
-	String firmware_version();
-	void set_mode(Report_mode mode);
-	void set_sleep(bool sleep);
-	bool query_data(int& pm25, int& pm10);
-	bool query_data_auto(int& pm25, int& pm10, int n);
-	bool timeout();
-	bool crc_ok();
+    Sds011(Stream& out);
+    bool device_info(String& firmware_version, String& device_id);
+    bool set_mode(Report_mode mode);
+    bool set_sleep(bool sleep);
+    bool query_data(int& pm25, int& pm10);
+    bool query_data(int& pm25, int& pm10, int n);
+    bool query_data_auto(int& pm25, int& pm10);
+    bool query_data_auto(int& pm25, int& pm10, int n);
+    bool timeout();
+    bool crc_ok();
 
 private:
-	void _send_cmd(enum Command cmd, const uint8_t* buf, uint8_t len);
-	uint8_t _read_byte(uint16_t timeout = 0);
-	String _buf_to_string();
-	void _ignore_response();
-	bool _read_response();
-	void _filter_data(int n, const int* pm25_table, const int* pm10_table, int& pm25, int& pm10);
+    void _send_cmd(enum Command cmd, const uint8_t* buf, uint8_t len);
+    uint8_t _read_byte(long unsigned deadline = 0);
+    String _buf_to_string();
+    void _ignore_response();
+    bool _read_response(enum Command cmd);
+    void _filter_data(int n, const int* pm25_table, const int* pm10_table, int& pm25, int& pm10);
 
-	Stream& _out;
-	uint8_t _buf[19];
-	bool _timeout = false;
+    Stream& _out;
+    uint8_t _buf[19];
+    bool _timeout = false;
 };
 
 #endif
